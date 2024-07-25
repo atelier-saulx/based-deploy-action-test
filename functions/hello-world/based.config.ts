76c957ff0494c4e677dc309ffb1097fa495cedbb
Luigui Delyer <git@s1x.com.br>
2024-07-25 13:32:10 +0200

    First commit

--- functions/hello-world/based.config.ts
+++ functions/hello-world/based.config.ts
@@ -1,11 +1,11 @@
 import { BasedFunctionConfig } from '@based/functions'
 
-        const config: BasedFunctionConfig = {
-          type: 'app',
-          name: 'hello-world',
-          public: true,
-          main: './app.ts',
-          path: '/',
-        }
+const config: BasedFunctionConfig = {
+  type: 'app',
+  name: 'hello-world',
+  public: true,
+  main: './app.ts',
+  path: '/',
+}
 
-        export default config
+export default config
