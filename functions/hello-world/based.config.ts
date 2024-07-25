b285f10c6f6afedffdbe61e2d43186ff2e90aa51
Luigui Delyer <git@s1x.com.br>
2024-07-25 13:29:54 +0200

    First commit

---
+++ functions/hello-world/based.config.ts
@@ -0,0 +1,11 @@
+import { BasedFunctionConfig } from '@based/functions'
+
+        const config: BasedFunctionConfig = {
+          type: 'app',
+          name: 'hello-world',
+          public: true,
+          main: './app.ts',
+          path: '/',
+        }
+
+        export default config
---
+++ functions/hello-world/index.ts
@@ -0,0 +1,18 @@
+import { BasedAppFunction } from '@based/functions';
+
+const app: BasedAppFunction = async (_based) => {
+  return '<!DOCTYPE html>' +
+    '<html lang="en">' +
+      '<head>' +
+        '<meta charset="utf-8" />' +
+        '<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0, viewport-fit=cover" />' +
+        '<meta name="title" content="Hello World" />' +
+        '<title>Hello World</title>' +
+      '</head>' +
+      '<body style="margin: 0;">' +
+        '<div id="root">Hello World from Based.io!</div>' +
+      '</body>' +
+    '</html>';
+};
+
+export default app;
