f65ff22503063e5ead70a1c3d7f319f85bc41fb4
Luigui Delyer <git@s1x.com.br>
2024-10-09 15:47:41 +0200

    push

--- functions/hello-world/index.ts
+++ functions/hello-world/index.ts
@@ -1,18 +1,20 @@
 import { BasedAppFunction } from '@based/functions';
 
 const app: BasedAppFunction = async (_based) => {
-  return '<!DOCTYPE html>' +
+  return (
+    '<!DOCTYPE html>' +
     '<html lang="en">' +
-      '<head>' +
-        '<meta charset="utf-8" />' +
-        '<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0, viewport-fit=cover" />' +
-        '<meta name="title" content="Hello World" />' +
-        '<title>Hello World</title>' +
-      '</head>' +
-      '<body style="margin: 0;">' +
-        `<div id="root">Hello World from the branch main in Based.io!</div>` +
-      '</body>' +
-    '</html>';
+    '<head>' +
+    '<meta charset="utf-8" />' +
+    '<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0, viewport-fit=cover" />' +
+    '<meta name="title" content="Hello World" />' +
+    '<title>Hello World</title>' +
+    '</head>' +
+    '<body style="margin: 0;">' +
+    `<div id="root">Hello World from the branch main 2 in Based.io!</div>` +
+    '</body>' +
+    '</html>'
+  );
 };
 
 export default app;
